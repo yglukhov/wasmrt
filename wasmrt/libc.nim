4eb4428d35c4ec7450f1a7d5aa934440b49016c5
Yuriy Glukhov <yuriy.glukhov@gmail.com>
2025-08-02 10:24:48 +0200

    Fixed clock_gettime nanoseconds

--- wasmrt/libc.nim
+++ wasmrt/libc.nim
@@ -19,7 +19,7 @@ proc gettimeImpl(): cint {.importwasmf: "Date.now".}
 proc clock_gettime(clkId: Clockid, tp: var Timespec): cint {.exportc.} =
   let t = gettimeImpl()
   tp.tv_sec = Time(t div 1000)
-  tp.tv_nsec = (t mod 1000) * 1000
+  tp.tv_nsec = (t mod 1000) * 1000000
 
 proc atan2Aux(a, b: float): float {.importwasmf: "Math.atan2".}
 proc atan2(a, b: cdouble): cdouble {.exportc.} = atan2Aux(a, b)
